479157ee7de5186071a1138b4c187c9439498339
Banty <bantyder@gmail.com>
2013-06-19 10:22:32 +0300

    CATS-306 Implement Generic Repository

--- Domain/DRMFSS.BLL/Repository/GenericRepository.cs
+++ Domain/DRMFSS.BLL/Repository/GenericRepository.cs
@@ -1,79 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Text;
 using DRMFSS.BLL.Interfaces;
 
 namespace DRMFSS.BLL.Repository
 {
-    public abstract class GenericRepository<C, T> :
-    IGenericRepository<T>
+    public class GenericRepository<T> :
+        IGenericRepository<T>
         where T : class
-        where C : DbContext//, new()
     {
 
-        private C _entities;// = new C();
-        public C db
+        private CTSContext _entities;
+
+        public CTSContext db
         {
 
             get { return _entities; }
             set { _entities = value; }
         }
 
-        public IUnitOfWork repository { get; set; }
+        public GenericRepository(CTSContext context)
+        {
+            _entities = context;
+        }
 
         public virtual List<T> GetAll()
         {
-
             IQueryable<T> query = _entities.Set<T>();
             return query.ToList();
         }
 
+
         public List<T> FindBy(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
         {
-
             IQueryable<T> query = _entities.Set<T>().Where(predicate);
             return query.ToList();
         }
+
         public virtual void Attach(T entity)
         {
             _entities.Set<T>().Attach(entity);
         }
-        
+
+
         public virtual bool Add(T entity)
         {
             _entities.Set<T>().Add(entity);
             return true;
         }
 
-        public virtual void Delete(T entity)
+
+        public virtual bool Delete(T entity)
         {
             _entities.Set<T>().Remove(entity);
-        }
-
-        public virtual void Edit(T entity)
-        {
-            _entities.Entry(entity).State = System.Data.EntityState.Modified;
-        }
-
-        public virtual void Save()
-        {
-            _entities.SaveChanges();
-        }
-
-        public virtual bool SaveChanges(T entity)
-        {
-            if (_entities.Entry(entity).State == EntityState.Detached)
-            {
-                _entities.Set<T>().Attach(entity);
-            }
-            _entities.Entry(entity).State = EntityState.Modified;
-            _entities.SaveChanges();
             return true;
         }
 
+        public virtual bool Edit(T entity)
+        {
+            _entities.Entry(entity).State = EntityState.Modified;
+            return true;
+        }
+
+        public virtual T FindById(int id)
+        {
+            return _entities.Set<T>().Find(id);
+        }
     }
 }
