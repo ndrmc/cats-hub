0abf036a50f8b04c4ee5a725da7b7750f4ae7d57
dawit <dawitdb@yahoo.com>
2013-06-20 15:01:09 +0300

    CATS - 322 Translation Service

---
+++ Domain/DRMFSS.BLL/Services/ITranslationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface ITranslationService
+    {
+        string GetForText(string text, string langauge);
+        List<Translation> GetAll(string languageCode);
+        bool DeleteByID(int id);
+        Translation FindById(int id);
+
+    }
+}
