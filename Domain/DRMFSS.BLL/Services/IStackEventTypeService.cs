a2a6d57cb83103fd33bdc7ca5cc6f6271fb9b06c
dawit <dawitdb@yahoo.com>
2013-06-21 11:05:44 +0300

    CATS - 326 Stack Event Type

---
+++ Domain/DRMFSS.BLL/Services/IStackEventTypeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface IStackEventTypeService
+    {
+        double GetFollowUpDurationByStackEventTypeId(int stackEventTypeId);
+        bool DeleteByID(int id);
+        StackEventType FindById(int id);
+
+    }
+}
---
+++ Domain/DRMFSS.BLL/Services/StackEventTypeService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public class StackEventTypeService:IStackEventTypeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StackEventTypeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public double GetFollowUpDurationByStackEventTypeId(int stackEventTypeId)
+        {
+            var followupDuration = _unitOfWork.StackEventTypeRepository.FindBy(s => s.StackEventTypeID == stackEventTypeId).Select(r => r.DefaultFollowUpDuration.Value).FirstOrDefault();
+            return Convert.ToDouble(followupDuration);
+        }
+
+        public bool DeleteByID(int id)
+        {
+            var stackType = _unitOfWork.StackEventTypeRepository.FindBy(s => s.StackEventTypeID == id).SingleOrDefault();
+            if (stackType == null) return false;
+            _unitOfWork.StackEventTypeRepository.Delete(stackType);
+            _unitOfWork.Save();
+            return true;
+        }
+
+        public StackEventType FindById(int id)
+        {
+            return _unitOfWork.StackEventTypeRepository.FindBy(s => s.StackEventTypeID == id).SingleOrDefault();
+        }
+    }
+}
