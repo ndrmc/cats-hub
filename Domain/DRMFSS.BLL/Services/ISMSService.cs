f354ee3bfafcbaba8f84c077ed31d56c631067ca
dawit <dawitdb@yahoo.com>
2013-06-21 14:20:04 +0300

    CATS-328 SMS Service

---
+++ Domain/DRMFSS.BLL/Services/ISMSService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface ISMSService
+    {
+        bool DeleteByID(int id);
+        SMS FindById(int id);
+
+    }
+}
---
+++ Domain/DRMFSS.BLL/Services/SMSService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public class SMSService:ISMSService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SMSService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public bool DeleteByID(int id)
+        {
+            var sms = _unitOfWork.SMSRepository.FindBy(s=>s.SMSID ==id).SingleOrDefault();
+            if (sms == null) return false;
+            _unitOfWork.SMSRepository.Delete(sms);
+            _unitOfWork.Save();
+            return true;
+        }
+
+        public SMS FindById(int id)
+        {
+          return  _unitOfWork.SMSRepository.FindBy(s=>s.SMSID ==id).SingleOrDefault();
+           
+        }
+    }
+}
