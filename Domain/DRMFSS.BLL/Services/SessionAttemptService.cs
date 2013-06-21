d8907b50497096a27dfc92db83fb357b67a25a1c
dawit <dawitdb@yahoo.com>
2013-06-21 17:22:40 +0300

    CATS-371 Session Attempts Service

---
+++ Domain/DRMFSS.BLL/Services/SessionAttemptService.cs
@@ -0,0 +1,75 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+
+namespace DRMFSS.BLL.Services
+{
+
+    public class SessionAttemptService : ISessionAttemptService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public SessionAttemptService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+        #region Default Service Implementation
+        public bool AddSessionAttempt(SessionAttempt entity)
+        {
+            _unitOfWork.SessionAttemptRepository.Add(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool EditSessionAttempt(SessionAttempt entity)
+        {
+            _unitOfWork.SessionAttemptRepository.Edit(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool DeleteSessionAttempt(SessionAttempt entity)
+        {
+            if (entity == null) return false;
+            _unitOfWork.SessionAttemptRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public bool DeleteById(int id)
+        {
+            var entity = _unitOfWork.SessionAttemptRepository.FindById(id);
+            if (entity == null) return false;
+            _unitOfWork.SessionAttemptRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public List<SessionAttempt> GetAllSessionAttempt()
+        {
+            return _unitOfWork.SessionAttemptRepository.GetAll();
+        }
+        public SessionAttempt FindById(int id)
+        {
+            return _unitOfWork.SessionAttemptRepository.FindById(id);
+        }
+        public List<SessionAttempt> FindBy(Expression<Func<SessionAttempt, bool>> predicate)
+        {
+            return _unitOfWork.SessionAttemptRepository.FindBy(predicate);
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+
+        }
+
+    }
+}
+
+ 
+      
