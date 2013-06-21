516073da33369986a732a646de6cd47b59ba2e04
dawit <dawitdb@yahoo.com>
2013-06-21 17:19:29 +0300

    CATS-370 Roles Service

---
+++ Domain/DRMFSS.BLL/Services/IRoleService.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface IRoleService
+    {
+
+        bool AddRole(RoleService entity);
+        bool DeleteRole(RoleService entity);
+        bool DeleteById(int id);
+        bool EditRole(RoleService entity);
+        RoleService FindById(int id);
+        List<RoleService> GetAllRole();
+        List<RoleService> FindBy(Expression<Func<RoleService, bool>> predicate);
+
+
+    }
+}
+
+
+      
