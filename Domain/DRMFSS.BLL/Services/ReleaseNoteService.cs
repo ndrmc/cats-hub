4315acb84acb5e83eb9f361586cb574df15a324c
dawit <dawitdb@yahoo.com>
2013-06-22 09:56:23 +0300

    CATS-369 Release Notes Service

---
+++ Domain/DRMFSS.BLL/Services/ReleaseNoteService.cs
@@ -0,0 +1,74 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+
+namespace DRMFSS.BLL.Services
+{
+
+    public class  ReleaseNoteService : IReleaseNoteService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public  ReleaseNoteService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+        #region Default Service Implementation
+        public bool AddReleaseNote( ReleaseNote entity)
+        {
+            _unitOfWork. ReleaseNoteRepository.Add(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool EditReleaseNote( ReleaseNote entity)
+        {
+            _unitOfWork. ReleaseNoteRepository.Edit(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool DeleteReleaseNote( ReleaseNote entity)
+        {
+            if (entity == null) return false;
+            _unitOfWork. ReleaseNoteRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public bool DeleteById(int id)
+        {
+            var entity = _unitOfWork. ReleaseNoteRepository.FindById(id);
+            if (entity == null) return false;
+            _unitOfWork. ReleaseNoteRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public List< ReleaseNote> GetAllReleaseNote()
+        {
+            return _unitOfWork. ReleaseNoteRepository.GetAll();
+        }
+        public  ReleaseNote FindById(int id)
+        {
+            return _unitOfWork. ReleaseNoteRepository.FindById(id);
+        }
+        public List< ReleaseNote> FindBy(Expression<Func< ReleaseNote, bool>> predicate)
+        {
+            return _unitOfWork. ReleaseNoteRepository.FindBy(predicate);
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+
+        }
+
+    }
+}
+
+
