9edb2fdebd28f010b2b0fed0c4b84fa51f63799f
dawit <dawitdb@yahoo.com>
2013-06-22 11:51:08 +0300

    CATS -336 Admin Unit Type

---
+++ Domain/DRMFSS.BLL/Services/AdminUnitService.cs
@@ -0,0 +1,74 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+
+namespace DRMFSS.BLL.Services
+{
+
+    public class AdminUnitService : IAdminUnitService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public AdminUnitService()
+        {
+            this._unitOfWork = new UnitOfWork();
+        }
+        #region Default Service Implementation
+        public bool AddAdminUnit(AdminUnit entity)
+        {
+            _unitOfWork.AdminUnitRepository.Add(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool EditAdminUnit(AdminUnit entity)
+        {
+            _unitOfWork.AdminUnitRepository.Edit(entity);
+            _unitOfWork.Save();
+            return true;
+
+        }
+        public bool DeleteAdminUnit(AdminUnit entity)
+        {
+            if (entity == null) return false;
+            _unitOfWork.AdminUnitRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public bool DeleteById(int id)
+        {
+            var entity = _unitOfWork.AdminUnitRepository.FindById(id);
+            if (entity == null) return false;
+            _unitOfWork.AdminUnitRepository.Delete(entity);
+            _unitOfWork.Save();
+            return true;
+        }
+        public List<AdminUnit> GetAllAdminUnit()
+        {
+            return _unitOfWork.AdminUnitRepository.GetAll();
+        }
+        public AdminUnit FindById(int id)
+        {
+            return _unitOfWork.AdminUnitRepository.FindById(id);
+        }
+        public List<AdminUnit> FindBy(Expression<Func<AdminUnit, bool>> predicate)
+        {
+            return _unitOfWork.AdminUnitRepository.FindBy(predicate);
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+
+        }
+
+    }
+}
+
+
---
+++ Domain/DRMFSS.BLL/Services/IAdminUnitService.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface IAdminUnitService
+    {
+
+        bool AddAdminUnit(AdminUnit entity);
+        bool DeleteAdminUnit(AdminUnit entity);
+        bool DeleteById(int id);
+        bool EditAdminUnit(AdminUnit entity);
+        AdminUnit FindById(int id);
+        List<AdminUnit> GetAllAdminUnit();
+        List<AdminUnit> FindBy(Expression<Func<AdminUnit, bool>> predicate);
+
+
+    }
+}
+
+
