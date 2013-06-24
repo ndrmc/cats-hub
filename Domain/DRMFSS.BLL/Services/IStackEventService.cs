c28fe13811402a0f79fe13067f7920fec33351f1
dawit <dawitdb@yahoo.com>
2013-06-24 09:53:11 +0300

    CATS-356,CATS-327,CATS-325 Create Itnernal Movement service, stack event service, store service

---
+++ Domain/DRMFSS.BLL/Services/IStackEventService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+   public interface IStackEventService
+    {
+
+       bool AddStackEvent(StackEvent entity);
+       bool DeleteStackEvent(StackEvent entity);
+       bool DeleteById(int id);
+       bool EditStackEvent(StackEvent entity);
+       StackEvent FindById(int id);
+       List<StackEvent> GetAllStackEvent();
+       List<StackEvent> FindBy(Expression<Func<StackEvent, bool>> predicate);
+
+       List<ViewModels.StackEventLogViewModel> GetAllStackEvents(UserProfile user);
+       List<ViewModels.StackEventLogViewModel> GetAllStackEventsByStoreIdStackId(UserProfile user, int StackId, int StoreId);
+      
+    }
+}
+
+      
+
+
+      
