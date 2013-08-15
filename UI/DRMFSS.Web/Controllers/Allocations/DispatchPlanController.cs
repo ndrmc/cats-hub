6d905a1fe71e48c452207c566afecab5ff7dd778
banty <bantyder@gmail.com>
2013-08-15 15:23:48 +0300

    Refactor DispatchPlanController and write unit test

--- UI/DRMFSS.Web/Controllers/Allocations/DispatchPlanController.cs
+++ UI/DRMFSS.Web/Controllers/Allocations/DispatchPlanController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DRMFSS.BLL.Services;
 
 namespace DRMFSS.Web.Controllers.Allocations
 {
     public class DispatchPlanController : BaseController
     {
+        private IDispatchAllocationService _dispatchAllocationService;
+
+        public DispatchPlanController(IDispatchAllocationService dispatchAllocationService)
+        {
+            this._dispatchAllocationService = dispatchAllocationService;
+        }
         //
         // GET: /DispatchPlan/
 
@@ -18,7 +25,7 @@ namespace DRMFSS.Web.Controllers.Allocations
 
         public ActionResult ListRequisitions()
         {
-            return PartialView(repository.DispatchAllocation.GetSummaryForUncommitedAllocations(GetCurrentUserProfile().DefaultHub.HubID));
+            return PartialView(_dispatchAllocationService.GetSummaryForUncommitedAllocations(GetCurrentUserProfile().DefaultHub.HubID));
         }
 
         public ActionResult RequistionDetails(int req)
