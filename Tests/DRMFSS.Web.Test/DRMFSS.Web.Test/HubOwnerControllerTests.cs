34e72236c2036fa6cb313be5e697106198dbd8c8
Beka Mengistu <bekalive@gmail.com>
2013-08-15 11:31:51 +0300

    HubOwner Controller

--- Tests/DRMFSS.Web.Test/DRMFSS.Web.Test/HubOwnerControllerTests.cs
+++ Tests/DRMFSS.Web.Test/DRMFSS.Web.Test/HubOwnerControllerTests.cs
@@ -11,37 +11,37 @@ using NUnit.Framework;
 namespace DRMFSS.Web.Test
 {
     [TestFixture]
-    public class ContactControllerTests
+    public class HubOwnerControllerTests
     {
         #region SetUp / TearDown
 
-        private ContactController _contactController;
+        private HubOwnerController _hubOwnerController;
         [SetUp]
         public void Init()
         {
-            var contacts = new List<Contact>
+            var hubOwners = new List<HubOwner>
                 {
-                    new Contact{ ContactID = 1,  FDPID = 1, FirstName = "Abebe", LastName = "Kebede", PhoneNo = "251116123456"},
-                    new Contact{ ContactID = 2, FDPID = 2, FirstName = "Abebech", LastName = "Zeleke", PhoneNo = "251911123456"}
+                    new HubOwner{HubOwnerID=1,LongName="Abebe Tesfaye",Name="Abebe"},
+                    new HubOwner{HubOwnerID=2,LongName="Tesfaye Deghu",Name="Tesfaye"}
                 };
-            var contactService = new Mock<IContactService>();
-            contactService.Setup(t => t.GetAllContact()).Returns(contacts);
-
+            var hubOwnerService = new Mock<IHubOwnerService>();
+            hubOwnerService.Setup(t => t.GetAllHubOwner()).Returns(hubOwners);
+            /*
             var fdps = new List<FDP>
                 {
                     new FDP{FDPID = 1, Name = "Elidar", AdminUnitID = 1},
                     new FDP{FDPID = 2, Name = "Gagu", AdminUnitID = 2},
                 };
             var fdpService = new Mock<IFDPService>();
-            fdpService.Setup(t => t.GetAllFDP()).Returns(fdps);
+            fdpService.Setup(t => t.GetAllFDP()).Returns(fdps);, fdpService.Object*/
 
-            _contactController = new ContactController(contactService.Object, fdpService.Object);
+            _hubOwnerController = new HubOwnerController(hubOwnerService.Object);
         }
 
         [TearDown]
         public void Dispose()
         {
-            _contactController.Dispose();
+            _hubOwnerController.Dispose();
         }
 
         #endregion
@@ -52,54 +52,50 @@ namespace DRMFSS.Web.Test
         public void CanViewIndex()
         {
             //ACT
-            var viewResult = _contactController.Index(1);
+            var viewResult = _hubOwnerController.Index();
 
             //ASSERT
             Assert.NotNull(viewResult);
             var model = viewResult.Model;
             Assert.NotNull(model);
-            Assert.IsInstanceOf<IEnumerable<Contact>>(model);
-            Assert.AreEqual(1, ((IEnumerable<Contact>)model).Count());
-            Assert.IsInstanceOf<Int32>(viewResult.ViewBag.FDPID);
-            Assert.IsInstanceOf<string>(viewResult.ViewBag.FDPName);
+            Assert.IsInstanceOf<IEnumerable<HubOwner>>(model);
+            Assert.AreEqual(1, ((IEnumerable<HubOwner>)model).Count());
+
         }
 
         [Test]
         public void CanViewDetails()
         {
             //ACT
-            var viewResult = _contactController.Details(1);
+            var viewResult = _hubOwnerController.Details(1);
 
             //ASSERT
             Assert.NotNull(viewResult);
             var model = viewResult.Model;
             Assert.NotNull(model);
-            Assert.IsInstanceOf<Contact>(model);
-            Assert.IsInstanceOf<Int32>(viewResult.ViewBag.FDPID);
-            Assert.IsInstanceOf<string>(viewResult.ViewBag.FDPName);
+            Assert.IsInstanceOf<HubOwner>(model);
+           
         }
 
         [Test]
         public void CanViewCreate()
         {
             //ACT
-            var viewResult = _contactController.Create(1) as ViewResult;
+            var viewResult = _hubOwnerController.Create(1) as ViewResult;
 
             //ASSERT
             Assert.NotNull(viewResult);
             var model = viewResult.Model;
             Assert.NotNull(model);
-            Assert.IsInstanceOf<Contact>(model);
-            Assert.IsInstanceOf<Int32>(viewResult.ViewBag.FDPID);
-            Assert.IsInstanceOf<string>(viewResult.ViewBag.FDPName);
+            Assert.IsInstanceOf<HubOwner>(model);
         }
 
         [Test]
         public void CanDoCreatePostBack()
         {
             //ACT
-            var contact = new Contact{ FDPID = 1, FirstName = "Kebede", LastName = "Molla", PhoneNo = "251116123456"};
-            var redirectToRouteResult = _contactController.Create(contact) as RedirectToRouteResult;
+            var hubOwner = new HubOwner { LongName = "Belay Zeleke", Name = "Beze" };
+            var redirectToRouteResult = _hubOwnerController.Create(hubOwner) as RedirectToRouteResult;
 
             //Assert
             Assert.NotNull(redirectToRouteResult);
@@ -111,25 +107,25 @@ namespace DRMFSS.Web.Test
         public void CanDoEditPostBack()
         {
             //ACT
-            var contact = new Contact { ContactID = 1, FDPID = 1, FirstName = "Kebede", LastName = "Molla", PhoneNo = "251116123456" };
-            var redirectToRouteResult = _contactController.Create(contact) as RedirectToRouteResult;
+            var hubOwner = new HubOwner { LongName = "Belay Zeleke", Name = "Beze" };
+            var redirectToRouteResult = _hubOwnerController.Create(hubOwner) as RedirectToRouteResult;
 
             //Assert
             Assert.NotNull(redirectToRouteResult);
             Assert.AreEqual("Index", redirectToRouteResult.RouteValues["Action"]);
-            Assert.AreEqual("Contact", redirectToRouteResult.RouteValues["controller"]);
+            Assert.AreEqual("HubOwner", redirectToRouteResult.RouteValues["controller"]);
         }
-
+        
         [Test]
         public void CanRedirectDeleteConfirm()
         {
             //ACt
-            var redirectToRouteResult = _contactController.DeleteConfirmed(1) as RedirectToRouteResult;
+            var redirectToRouteResult = _hubOwnerController.DeleteConfirmed(1) as RedirectToRouteResult;
 
             //Assert
             Assert.NotNull(redirectToRouteResult);
             Assert.AreEqual("Index", redirectToRouteResult.RouteValues["Action"]);
-            Assert.AreEqual("Contact", redirectToRouteResult.RouteValues["controller"]);
+            Assert.AreEqual("HubOwner", redirectToRouteResult.RouteValues["controller"]);
         }
 
 
