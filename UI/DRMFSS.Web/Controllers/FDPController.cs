eacd473d8d3ca2c6a084e1bfe00ba3f6bc9743b0
Beka Mengistu <bekalive@gmail.com>
2013-08-15 07:14:57 +0300

    FDP Controller and Test

--- UI/DRMFSS.Web/Controllers/FDPController.cs
+++ UI/DRMFSS.Web/Controllers/FDPController.cs
@@ -1,198 +1,75 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using DRMFSS.BLL;
-using Telerik.Web.Mvc;
-using System.Collections.Generic;
-using DRMFSS.BLL.Repository;
+using DRMFSS.BLL.Services;
 
 namespace DRMFSS.Web.Controllers
-{ 
-    public class FDPController : BaseController
+{
+    [Authorize]
+    public partial class FDPController : BaseController
     {
 
-        public FDPController(IUnitOfWork repository)
-        {
-            this.repository = repository;
-        }
+        private readonly IFDPService _FDPService;
 
-        public FDPController()
+        public FDPController(IFDPService FDPServiceParam)
         {
-           
+            this._FDPService = FDPServiceParam;
         }
-
         //
         // GET: /FDP/
 
-        public ViewResult Index()
+        public virtual ViewResult Index()
         {
-
-            ViewBag.Regions = repository.AdminUnit.GetRegions();
-            var fdps = repository.FDP.GetAll();
-            var list =
-                fdps.OrderBy(f => f.AdminUnit.AdminUnit2.AdminUnit2.Name).ThenBy(f => f.AdminUnit.AdminUnit2.Name).
-                    ThenBy(f => f.AdminUnit.Name).ThenBy(f => f.Name);
-            return View("Index", list);
+            var fdps = _FDPService.GetAllFDP();
+            return View(fdps);
         }
 
-        public ActionResult _FDPPartial()
-        {
-
-            ViewBag.Regions = repository.AdminUnit.GetRegions();
-            var fdps = repository.FDP.GetAll();
-            var list =
-                fdps.OrderBy(f => f.AdminUnit.AdminUnit2.AdminUnit2.Name).ThenBy(f => f.AdminUnit.AdminUnit2.Name).
-                    ThenBy(f => f.AdminUnit.Name).ThenBy(f => f.Name);
-            return PartialView("Index", list);
-        }
-
-        public ActionResult IndexPartial()
-        {
-
-            ViewBag.Regions = repository.AdminUnit.GetRegions();
-            var fdps = repository.FDP.GetAll();
-            var list =
-                fdps.OrderBy(f => f.AdminUnit.AdminUnit2.AdminUnit2.Name).ThenBy(f => f.AdminUnit.AdminUnit2.Name).
-                    ThenBy(f => f.AdminUnit.Name).ThenBy(f => f.Name);
-            return PartialView("IndexPartial", list);
-        }
-
-        public ActionResult Update()
-        {
-            ViewBag.Regions = repository.AdminUnit.GetRegions();
-            var fdps = repository.FDP.GetAll();
-            var list =
-                fdps.OrderBy(f => f.AdminUnit.AdminUnit2.AdminUnit2.Name).ThenBy(f => f.AdminUnit.AdminUnit2.Name).
-                    ThenBy(f => f.AdminUnit.Name).ThenBy(f => f.Name);
-            return PartialView(list);
-        }
-
-        public ActionResult GetFDPs(int? woredaId)
-        {
-            // Check if the request came in a different Spelling than the parameter,
-            if (woredaId == null && Request["WoredaID"] != null)
-            {
-                woredaId = System.Convert.ToInt32(Request["WoredaID"]);
-            }
-
-            if (woredaId != null)
-            {
-                var fdps = from p in repository.FDP.GetAll()
-                           where p.AdminUnitID == woredaId
-                           select new Models.AdminUnitItem() { Id = p.FDPID, Name = p.Name };
-                return Json(new SelectList(fdps.OrderBy(f => f.Name), "Id", "Name"), JsonRequestBehavior.AllowGet);
-            }
-            return Json(new SelectList(new List<Models.AdminUnitItem>(),"Id","Name"), JsonRequestBehavior.AllowGet);
-        }
-
-        public ActionResult GetFDPsReport(int? woredaId)
-        {
-            if (woredaId != null)
-            {
-                var fdps = (from p in repository.FDP.GetAll()
-                           where p.AdminUnitID == woredaId
-                           select new Models.AdminUnitItem() { Id = p.FDPID, Name = p.Name }).ToList();
-                fdps.Insert(0, new Models.AdminUnitItem { Name = "All" });
-                return Json(new SelectList(fdps.OrderBy(f => f.Name), "Id", "Name"), JsonRequestBehavior.AllowGet);
-            }
-            return Json(new SelectList(new List<Models.AdminUnitItem>(), "Id", "Name"), JsonRequestBehavior.AllowGet);
-        }
-
-        [GridAction(EnableCustomBinding = false)]
-        public ActionResult GetFDPGrid(int? woredaId, int? zoneId, int? regionId)
-        {
-            List<BLL.FDP> fdps = new List<BLL.FDP>();
-            if (woredaId.HasValue)
-            {
-                fdps = repository.FDP.GetFDPsByWoreda(woredaId.Value);
-            }
-            else if (zoneId.HasValue)
-            {
-                fdps = repository.FDP.GetFDPsByZone(zoneId.Value);
-            }
-            else if (regionId.HasValue)
-            {
-                fdps = repository.FDP.GetFDPsByRegion(regionId.Value);
-            }else
-            {
-                fdps = repository.FDP.GetAll();
-            }
-            var f = from p in fdps
-                    select new
-                    {
-                        FDPID = p.FDPID,
-                        Name = p.Name,
-                        NameAM = p.NameAM,
-                        AdminUnit = new
-                        {
-                            Name = p.AdminUnit.Name,
-                            AdminUnit2 = new
-                            {
-                                Name = p.AdminUnit.AdminUnit2.Name,
-                                AdminUnit2 = new 
-                                {
-                                    Name = p.AdminUnit.AdminUnit2.AdminUnit2.Name
-                                }
-                            }
-                        }
-                    };
-           var result =  f.OrderBy(o => o.AdminUnit.AdminUnit2.AdminUnit2.Name).ThenBy(o => o.AdminUnit.AdminUnit2.Name).ThenBy(
-                o => o.AdminUnit.Name).ThenBy(o => o.Name);
-            return View(new GridModel(result));
-        }
 
         //
         // GET: /FDP/Details/5
 
-        public ViewResult Details(int id)
+        public virtual ViewResult Details(int id)
         {
-            FDP fdp = repository.FDP.FindById(id);
+            FDP fdp = _FDPService.FindById(id);
             return View(fdp);
         }
 
         //
         // GET: /FDP/Create
 
-        public ActionResult Create(int woredaId)
+        public virtual ActionResult Create()
         {
-            Models.AdminUnitModel model = new Models.AdminUnitModel();
-            BLL.AdminUnit woreda = repository.AdminUnit.FindById(woredaId);
-            model.SelectedWoredaId = woreda.AdminUnitID;
-            model.SelectedWoredaName = woreda.Name;
-            model.SelectedZoneId = woreda.AdminUnit2.AdminUnitID;
-            model.SelectedZoneName = woreda.AdminUnit2.Name;
-            model.SelectedRegionId = woreda.AdminUnit2.AdminUnit2.AdminUnitID;
-            model.SelectedRegionName = woreda.AdminUnit2.AdminUnit2.Name;
-
-            return PartialView("Create", model);
-        } 
+            return PartialView();
+        }
 
         //
         // POST: /FDP/Create
 
         [HttpPost]
-        public ActionResult Create(Models.AdminUnitModel unit)
+        public virtual ActionResult Create(FDP fdp)
         {
             if (ModelState.IsValid)
             {
-                BLL.FDP fdp = new FDP()
-                                  {AdminUnitID = unit.SelectedWoredaId, Name = unit.UnitName, NameAM = unit.UnitNameAM};
-                if (ModelState.IsValid)
-                {
-                    repository.FDP.Add(fdp);
-                    return Json(new {success = true});
-                }
+                _FDPService.AddFDP(fdp);
+                return Json(new { success = true });
             }
-            // ViewBag.AdminUnitID = new SelectList(new BLL.Repository.AdminUnitRepository().GetAllWoredas(), "AdminUnitID", "Name", fdp.AdminUnitID);
-            return PartialView("Create", unit);
+
+            return PartialView(fdp);
         }
-        
+
         //
         // GET: /FDP/Edit/5
- 
-        public ActionResult Edit(int id)
+
+        public virtual ActionResult Edit(int id)
         {
-            FDP fdp = repository.FDP.FindById(id);
-           // ViewBag.AdminUnitID = new SelectList(db.AdminUnits, "AdminUnitID", "Name", fdp.AdminUnitID);
+            FDP fdp = _FDPService.FindById(id);
+            var fdps = _FDPService.GetAllFDP();
+            ViewBag.FDPID = new SelectList(fdps, "FDPID", "Name", fdp.FDPID);
             return PartialView(fdp);
         }
 
@@ -200,34 +77,45 @@ namespace DRMFSS.Web.Controllers
         // POST: /FDP/Edit/5
 
         [HttpPost]
-        public ActionResult Edit(FDP fdp)
+        public virtual ActionResult Edit(FDP fdp)
         {
             if (ModelState.IsValid)
             {
-                repository.FDP.SaveChanges(fdp);
+
+                _FDPService.EditFDP(fdp);
+
+
+                //return RedirectToAction("Index");
                 return Json(new { success = true });
             }
-            //ViewBag.AdminUnitID = new SelectList(db.AdminUnits, "AdminUnitID", "Name", fdp.AdminUnitID);
-            return Json(new { success = false});
+
+            ViewBag.FDPID = new SelectList(_FDPService.GetAllFDP(), "FDPID", "Name", fdp.FDPID);
+            return PartialView(fdp);
         }
 
         //
         // GET: /FDP/Delete/5
- 
-        public ActionResult Delete(int id)
+
+        public virtual ActionResult Delete(int id)
         {
-            FDP fdp = repository.FDP.FindById(id);
-            return View("Delete", fdp);
+            FDP fdp = _FDPService.FindById(id);
+            return View(fdp);
         }
 
         //
         // POST: /FDP/Delete/5
 
         [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
+        public virtual ActionResult DeleteConfirmed(int id)
         {
-            repository.FDP.DeleteByID(id);
+            _FDPService.DeleteById(id);
             return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _FDPService.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
