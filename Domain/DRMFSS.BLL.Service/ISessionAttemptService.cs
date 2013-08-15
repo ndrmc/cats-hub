334c2d9b66ade7ec1bc9ddb94c0e17d4555566d4
nathnael <natash1145@gmail.com>
2013-08-15 14:26:52 +0300

    HubController Refactored and Tested

---
+++ Domain/DRMFSS.BLL.Service/ISessionAttemptService.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DRMFSS.BLL.Services
+{
+    public interface ISessionAttemptService
+    {
+
+        bool AddSessionAttempt(SessionAttempt entity);
+        bool DeleteSessionAttempt(SessionAttempt entity);
+        bool DeleteById(int id);
+        bool EditSessionAttempt(SessionAttempt entity);
+        SessionAttempt FindById(int id);
+        List<SessionAttempt> GetAllSessionAttempt();
+        List<SessionAttempt> FindBy(Expression<Func<SessionAttempt, bool>> predicate);
+
+
+    }
+}
+
+
