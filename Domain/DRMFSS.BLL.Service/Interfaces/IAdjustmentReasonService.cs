07c36a8f24b0b1145ab1d9a04846337680253a0c
banty <bantyder@gmail.com>
2013-08-16 17:18:26 +0300

    Merge master

--- Domain/DRMFSS.BLL.Service/Interfaces/IAdjustmentReasonService.cs
+++ Domain/DRMFSS.BLL.Service/Interfaces/IAdjustmentReasonService.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,19 +6,22 @@ using System.Text;
 
 namespace DRMFSS.BLL.Services
 {
-    public interface IAdjustmentReasonService
+    public interface IAdjustmentReasonService:IDisposable
     {
 
-        bool AddAdjustmentReason(AdjustmentReason adjustmentReason);
-        bool DeleteAdjustmentReason(AdjustmentReason adjustmentReason);
-        bool DeleteById(int id);
+         bool AddAdjustmentReason(AdjustmentReason adjustmentReason);
+
         bool EditAdjustmentReason(AdjustmentReason adjustmentReason);
-        AdjustmentReason FindById(int id);
+
+         bool DeleteAdjustmentReason(AdjustmentReason adjustmentReason);
+
+        bool DeleteById(int id);
+
         List<AdjustmentReason> GetAllAdjustmentReason();
+
+        AdjustmentReason FindById(int id);
+
         List<AdjustmentReason> FindBy(Expression<Func<AdjustmentReason, bool>> predicate);
-
-
+   
     }
 }
-
-
